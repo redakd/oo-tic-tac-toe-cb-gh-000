0bbdc1204fc8a0b7551857f26c9c86a1c37a7c3d
Rida Kodad <kodadreda@gmail.com>
2019-08-08 18:20:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,7 @@
 class TicTacToe
 
-  def initialize(@board = nil)
-    @board = @board || Array.new(9, " ")
+  def initialize(@board = Array.new(9, " ") )
+    @board = @board || 
   end
 
   WIN_COMBINATIONS = [
